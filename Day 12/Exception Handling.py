a8939564a2c8a0e4dd4b636328d6719b727ae6d5
R Santhosh Vid <153453429+rsanthosh1996@users.noreply.github.com>
2026-07-21 17:11:56 +0530

    Exception Handling

---
+++ Day 12/Exception Handling.py
@@ -0,0 +1,5 @@
+#Exception Handling errors in python , converting technical errors to user friendly msgs
+#errors - runtime, compile time, logical errors
+#compile time errors  which come during development, example syntax errors, runtime occurs when program is executing example - Valueerror,Typeerror
+#
+
